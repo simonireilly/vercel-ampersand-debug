944c95918a25cf49abca22f4873c57bdd1be3095
simon <simonireilly@gmail.com>
2020-10-12 17:36:28 +0100

    first commit

---
+++ pages/[...index].js
@@ -0,0 +1,30 @@
+import { useRouter } from 'next/router'
+
+export const getStaticPaths = () => {
+ return {
+   paths: [],
+   fallback: true
+ }
+}
+
+export const getStaticProps = () => {
+  return {
+    props: {},
+    revalidate: 1
+  }
+}
+
+
+export default function Home() {
+  const router = useRouter()
+
+  const { pathname, query } = router
+
+  return(
+  <div>
+    <pre>
+      {JSON.stringify({ pathname, query }, undefined, 2)}
+    </pre>
+  </div>
+  )
+}
